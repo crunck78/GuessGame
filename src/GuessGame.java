65c6c4686f464958f8bcd4cb597445186c985ffd
Crunck78 <crunck78@googlemail.com>
2021-06-25 14:21:57 +0200

    first commit

---
+++ src/GuessGame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class GuessGame {
+    private JPanel mainPanel;
+    private JLabel tryCounter;
+    private JLabel guessResult;
+    private JButton takeGuess;
+    private JTextField numberInput;
+}
